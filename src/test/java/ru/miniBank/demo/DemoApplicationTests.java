cdf2b6273bf61f1a2a5c8a3ed966f8a67f97955a
ViktoriaGatsulia <Viktoria.gatsulia@gmail.com>
2020-08-10 14:57:54 +0700

    Service norm work

--- src/test/java/ru/miniBank/demo/DemoApplicationTests.java
+++ src/test/java/ru/miniBank/demo/DemoApplicationTests.java
@@ -48,6 +48,60 @@ class DemoApplicationTests {
         assertThat(this.bankUserService.findById(1L).get().getBankCards()).isEqualTo(bankCards);
     }
 
+    @Test
+    public void testFindBankCardById() {
+        BankCard bankCard = new BankCard(1L, new Date(), 123, "1234 1234 1234 1234", 100D);
+        given(this.bankCardService.findById(1L)).willReturn(java.util.Optional.of(bankCard));
+        assertThat(this.bankCardService.findById(1L).get().getBalance()).isEqualTo(100D);
+    }
+
+//    @Test
+//    public void testViewShowTask() throws Exception {
+//        this.mvc
+//                .perform(get("/show_task"))
+//                .andExpect(status().isOk())
+//                .andExpect(content()
+//                        .contentType(MediaType.APPLICATION_JSON));
+//    }
+//
+//    @Test
+//    public void testViewShowUser() throws Exception {
+//        this.mvc
+//                .perform(get("/show_user"))
+//                .andExpect(status().isOk())
+//                .andExpect(content()
+//                        .contentType(MediaType.APPLICATION_JSON));
+//    }
+//
+//
+//    @Test
+//    public void controllerTest() throws Exception {
+//        given(this.userService.findById(1L))
+//                .willReturn(java.util.Optional.of(new User(1L, "Viktoria")));
+//        this.mvc.perform(get("/user_id=1").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk()).andExpect(content().
+//                string("{\"user_id\":1,\"fio\":\"Viktoria\",\"task_list\":[]}"));
+//    }
+//
+//    @Test
+//    public void testReturnTimeSpentOnWorkNotFound() throws Exception {
+//        this.mvc.perform(get("/time_spent_on_work_for_user=1").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isNotFound());
+//    }
+//
+//    @Test
+//    public void testReturnTimeSpentOnWorkFound() throws Exception {
+//        User user = new User(1L, "Viki");
+//        user.addTask(new Task(user, 1L, "title1", "desc1", new Date(20000)));
+//        user.addTask(new Task(user, 2L, "title2", "desc2", new Date(5000)));
+//        user.addTask(new Task(user, 3L, "title3", "desc3", new Date(10000)));
+//        given(this.userService.findById(1L)).willReturn(java.util.Optional.of(user));
+//        this.mvc.perform(get("/time_spent_on_work_for_user=1").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk()).andExpect(content().
+//                string("[{\"masterUser\":{\"user_id\":1,\"fio\":\"Viki\"},\"task_id\":1,\"title\":\"title1\",\"description\":\"desc1\",\"date_add_task\":\"2020-07-28T05:54:26.189+00:00\",\"date_start_task\":null,\"date_stop_task\":null,\"time\":\"1970-01-01T00:00:20.000+00:00\"},{\"masterUser\":{\"user_id\":1,\"fio\":\"Viki\"},\"task_id\":2,\"title\":\"title2\",\"description\":\"desc2\",\"date_add_task\":\"2020-07-28T05:54:26.189+00:00\",\"date_start_task\":null,\"date_stop_task\":null,\"time\":\"1970-01-01T00:00:05.000+00:00\"},{\"masterUser\":{\"user_id\":1,\"fio\":\"Viki\"},\"task_id\":3,\"title\":\"title3\",\"description\":\"desc3\",\"date_add_task\":\"2020-07-28T05:54:26.189+00:00\",\"date_start_task\":null,\"date_stop_task\":null,\"time\":\"1970-01-01T00:00:10.000+00:00\"}]"));
+//
+//    }
+
 }
 /*
  */
